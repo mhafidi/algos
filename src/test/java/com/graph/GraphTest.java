c7a98e189f793b772744de5c53b2889f74985007
mhafidi <m.hafidi85@gmail.com>
2022-03-11 13:01:49 -0800

    Cycle Computing

--- src/test/java/com/graph/GraphTest.java
+++ src/test/java/com/graph/GraphTest.java
@@ -5,9 +5,12 @@ import com.utils.ReadFile;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
 
 public class GraphTest {
     @Test
@@ -23,10 +26,12 @@ public class GraphTest {
     @Test
     public void testComputeCycles()
     {
-        String json=new ReadFile("src/main/resources/graph_PANCAKESWAPV2.json").getContent();
+        String json=new ReadFile("src/main/resources/graph_PANCAKESWAPV2_extended.json").getContent();
         Graph graph= new Graph(json);
         HashMap hashMap=graph.getCycles();
-        System.out.println(hashMap);
+        //System.out.println(hashMap);
+        List list= (List) hashMap.keySet().stream().filter(key->((List) hashMap.get(key)).size()>2).map(key->hashMap.get(key)).collect(Collectors.toList());
+        System.out.println(list.size());
     }
 
     //[["1","2"],["2","3"],["3","4"],["4","6"],["4","7"],["5","6"],["3","5"],["7","8"],["6","10"],["5","9"],["10","11"],["11","12"],["11","13"],["12","13"]]
